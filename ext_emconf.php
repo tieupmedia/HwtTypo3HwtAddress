dea187c5cc640d017b07404275800ed38f6239d1
Heiko Westermann <hwt3@gmx.de>
2016-06-23 01:22:11 +0200

    Version 0.0.4-beta: Updated emconf and changelog.

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Modern Address',
-	'description' => 'Address handling for TYPO3 >= 6.1',
+	'description' => 'Address handling for TYPO3 >= 6.2',
 	'category' => 'plugin',
 	'author' => 'Heiko Westermann',
 	'author_email' => 'hwt3@gmx.de',
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'conflicts' => '',
 	'priority' => '',
 	'module' => '',
-	'state' => 'alpha',
+	'state' => 'beta',
 	'internal' => '',
 	'uploadfolder' => 0,
 	'createDirs' => '',
@@ -29,17 +29,16 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.3',
+	'version' => '0.0.4',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.1.0-6.2.99',
+			'typo3' => '6.2.0-6.2.99',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
+			'ke_search' => '',
 		),
 	),
 	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"80be";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"d440";s:10:"README.txt";s:4:"ee2d";s:19:"doc/wizard_form.dat";s:4:"4688";s:20:"doc/wizard_form.html";s:4:"c8db";s:13:"mod1/conf.php";s:4:"691e";s:14:"mod1/index.php";s:4:"bdf5";s:18:"mod1/locallang.xml";s:4:"b5bc";s:22:"mod1/locallang_mod.xml";s:4:"db06";s:19:"mod1/moduleicon.gif";s:4:"8074";}',
 );
-
-?>

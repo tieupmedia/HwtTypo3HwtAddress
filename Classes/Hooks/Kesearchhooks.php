bd8492ebd62dbfc96113300f8901d0139f970771
Heiko Westermann <hwt3@gmx.de>
2016-06-23 01:37:19 +0200

    Task: Formatted code of ke_search hook

--- Classes/Hooks/Kesearchhooks.php
+++ Classes/Hooks/Kesearchhooks.php
@@ -36,13 +36,13 @@ class KeSearchHooks {
     /**
      * indexer configurations
      *
-	 * Adds the custom indexer to the TCA of indexer configurations, so that
-	 * it's selectable in the backend as an indexer type when you create a
-	 * new indexer configuration.
-	 *
-	 * @param array $params
-	 * @param type $pObj
-	 */
+     * Adds the custom indexer to the TCA of indexer configurations, so that
+     * it's selectable in the backend as an indexer type when you create a
+     * new indexer configuration.
+     *
+     * @param array $params
+     * @param type $pObj
+     */
     function registerIndexerConfiguration(&$params, $pObj) {
 
         // add address indexer item to "type" field
@@ -56,10 +56,10 @@ class KeSearchHooks {
         // enable "sysfolder" field
         $GLOBALS['TCA']['tx_kesearch_indexerconfig']['columns']['sysfolder']['displayCond'] .= ',hwtaddressindexer';
     }
-    
-    
+
+
     /**
-    * Food indexer
+    * Address indexer
     *
     * @param array $indexerConfig Configuration from TYPO3 Backend
     * @param array $indexerObject Reference to indexer class.
@@ -69,49 +69,49 @@ class KeSearchHooks {
         /*
          * address indexing
          */
-		if($indexerConfig['type'] == 'hwtaddressindexer') {
-			$content = '';
- 
-				// get all the entries to index
-				// don't index hidden or deleted elements, BUT
-				// get the elements with frontend user group access restrictions
-				// or time (start / stop) restrictions.
-				// Copy those restrictions to the index.
-			$fields = '*';
-			$table = 'tx_hwtaddress_domain_model_address';
-			$where = 'pid IN (' . $indexerConfig['sysfolder'] . ') AND hidden = 0 AND deleted = 0';
-			$groupBy = '';
-			$orderBy = '';
-			$limit = '';
-			$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery($fields,$table,$where,$groupBy,$orderBy,$limit);
- 
-			$resCount = $GLOBALS['TYPO3_DB']->sql_num_rows($res);
+        if($indexerConfig['type'] == 'hwtaddressindexer') {
+            $content = '';
+
+            // get all the entries to index
+            // don't index hidden or deleted elements, BUT
+            // get the elements with frontend user group access restrictions
+            // or time (start / stop) restrictions.
+            // Copy those restrictions to the index.
+            $fields = '*';
+            $table = 'tx_hwtaddress_domain_model_address';
+            $where = 'pid IN (' . $indexerConfig['sysfolder'] . ') AND hidden = 0 AND deleted = 0';
+            $groupBy = '';
+            $orderBy = '';
+            $limit = '';
+            $res = $GLOBALS['TYPO3_DB']->exec_SELECTquery($fields,$table,$where,$groupBy,$orderBy,$limit);
+
+            $resCount = $GLOBALS['TYPO3_DB']->sql_num_rows($res);
  
             // Loop through the records and write them to the index.
-			if($resCount) {
-				while ( ($record = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res)) ) {
+            if($resCount) {
+                while ( ($record = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res)) ) {
  
-						// compile the information which should go into the index
-						// the field names depend on the table you want to index!
-					$title = strip_tags($record['firstname'] . "\n" . $record['lastname'] . "\n" . $record['company_title']);
-					$abstract = strip_tags($record['position'] . "\n" . $record['department'] . "\n" . $record['company_subtitle'] . "\n" . $record['company_short']);
-					$content = strip_tags($record['info'] . "\n" . $record['company_bodytext']);
-					$fullContent = $title . "\n" . $abstract . "\n" . $content;
-					$params = '&tx_hwtaddress_address[address]=' . $record['uid'];
-					//$tags = '#example_tag_1#,#example_tag_2#';
-					$additionalFields = array(
+                    // compile the information which should go into the index
+                    // the field names depend on the table you want to index!
+                    $title = strip_tags($record['firstname'] . "\n" . $record['lastname'] . "\n" . $record['company_title']);
+                    $abstract = strip_tags($record['position'] . "\n" . $record['department'] . "\n" . $record['company_subtitle'] . "\n" . $record['company_short']);
+                    $content = strip_tags($record['info'] . "\n" . $record['company_bodytext']);
+                    $fullContent = $title . "\n" . $abstract . "\n" . $content;
+                    $params = '&tx_hwtaddress_address[address]=' . $record['uid'];
+                    //$tags = '#example_tag_1#,#example_tag_2#';
+                    $additionalFields = array(
                         'sortdate' => $record['crdate'],
                         'orig_uid' => $record['uid'],
                         'orig_pid' => $record['pid'],
                         'sortdate' => $record['datetime'],
                     );
  
-						// add something to the title, just to identify the entries
-						// in the frontend
-					$title = $title;
- 
-						// ... and store the information in the index
-					$indexerObject->storeInIndex(
+                    // add something to the title, just to identify the entries
+                    // in the frontend
+                    $title = $title;
+
+                    // ... and store the information in the index
+                    $indexerObject->storeInIndex(
                         $indexerConfig['storagepid'], // storage PID
                         $title, // record title
                         'hwtaddressindexer', // content type
@@ -126,11 +126,11 @@ class KeSearchHooks {
                         $record['fe_group'], // fe_group
                         false, // debug only?
                         $additionalFields // additionalFields
-				    );
-				}
-				$content = '<p><b>HWT Address Indexer "' . $indexerConfig['title'] . '": ' . $resCount . 'Elements have been indexed.</b></p>';
-			}
-			return $content;
-		}
-	}
+                    );
+                }
+                $content = '<p><b>HWT Address Indexer "' . $indexerConfig['title'] . '": ' . $resCount . 'Elements have been indexed.</b></p>';
+            }
+            return $content;
+        }
+    }
 }

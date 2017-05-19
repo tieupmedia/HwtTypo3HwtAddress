f2fb3a5111a36e2d3978eefc18d609f677e59573
Christoph Lehmann <lehmann@networkteam.com>
2017-05-19 09:53:20 +0200

    Feature: Add template switch in plugin
    You can register a new template with `$GLOBALS['TYPO3_CONF_VARS']['EXT']['hwt_address']['templates']['alternate'] = 'Alternate';` to render the partial Alternate.html

---
+++ Classes/Helper/TcaHelper.php
@@ -0,0 +1,33 @@
+<?php
+namespace Hwt\HwtAddress\Helper;
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
+
+class TcaHelper {
+
+	/**
+	 * Get list of templates
+	 *
+	 * @param array $params array of select field options (reference)
+	 * @param object $pObj parent object (reference)
+	 * @return void
+	 */
+	public function getTemplateList(&$params, $pObj) {
+		// Add default item
+		$params['items'][0] = [
+			0 => LocalizationUtility::translate('LLL:EXT:hwt_address/Resources/Private/Language/locallang_be.xlf:flexform_setting.template.default', 'hwtAddress'),
+			1 => 'Default'
+		];
+
+		$templateRegistry = $GLOBALS['TYPO3_CONF_VARS']['EXT']['hwt_address']['templates'];
+		if (isset($templateRegistry) && count($templateRegistry) > 0) {
+			foreach ($GLOBALS['TYPO3_CONF_VARS']['EXT']['hwt_address']['templates'] as $item => $name) {
+				$params['items'][] = [
+					0 => $name,
+					1 => GeneralUtility::underscoredToUpperCamelCase($item)
+				];
+			}
+		}
+	}
+}

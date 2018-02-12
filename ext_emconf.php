7e472632ba2c9a2f71d94911e1c7ab737d5d711d
tieupmedia <hwt3@gmx.de>
2018-02-12 16:01:41 +0100

    [BUGFIX] Corrected php version dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = array(
     'constraints' => array(
         'depends' => array(
             'typo3' => '7.6.0-8.7.99',
-            'php' => '5.3.0-7.1.99',
+            'php' => '5.4.0-7.2.99',
         ),
         'conflicts' => array(
         ),

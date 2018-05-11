d3c8913ca77ea87cc676806b021c1854ea70a686
tieupmedia <hwt3@gmx.de>
2018-05-11 09:17:54 +0200

    [UPDATE] Updated ce wizard configuration.

--- ext_localconf.php
+++ ext_localconf.php
@@ -37,19 +37,31 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['proc
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['list_type_Info']['hwtaddress_address'][$_EXTKEY] = 'Hwt\\HwtAddress\\Hooks\\CmsLayout->getExtensionSummary';
 
 
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
+    <INCLUDE_TYPOSCRIPT: source="FILE:EXT:hwt_address/Configuration/TSconfig/ContentElementWizard/mod.wizards.txt">
+');
+
+
 if (TYPO3_MODE === 'BE') {
     /*
      * Register folder icon
      * For TYPO3 >= 7.5, older version just need tca.php codes
      */
     if ( version_compare(TYPO3_version, '7.5.0') >= 0 ) {
+        $icons = array(
+            'apps-pagetree-folder-contains-hwtaddress' => 'folder-hwtaddress.gif',
+            'ext-hwtaddress-wizard-icon' => 'ce_wiz.gif',
+        );
+        
         /** @var \TYPO3\CMS\Core\Imaging\IconRegistry $iconRegistry */
         $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Imaging\\IconRegistry');
 
-        $iconRegistry->registerIcon(
-            'apps-pagetree-folder-contains-hwtaddress',
-            'TYPO3\\CMS\\Core\\Imaging\\IconProvider\\BitmapIconProvider',
-            array('source' => 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/folder-hwtaddress.gif')
-        );
+        foreach ($icons as $identifier => $file) {
+            $iconRegistry->registerIcon(
+                $identifier,
+                'TYPO3\\CMS\\Core\\Imaging\\IconProvider\\BitmapIconProvider',
+                array('source' => 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/' . $file)
+            );
+        }
     }
 }

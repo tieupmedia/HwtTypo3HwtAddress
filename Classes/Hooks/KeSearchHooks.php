6a43b0241165d93e54a3971192470017ce79dcb9
tieupmedia <hwt3@gmx.de>
2018-05-11 13:43:53 +0200

    [TASK] Migrated $GLOBALS['TYPO3_DB'] functions to QueryBuilder in ke_search indexer hook

--- Classes/Hooks/KeSearchHooks.php
+++ Classes/Hooks/KeSearchHooks.php
@@ -2,10 +2,15 @@
 
 namespace Hwt\HwtAddress\Hooks;
 
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Database\Query\QueryBuilder;
+//use TYPO3\CMS\Core\Database\Query\Restriction\FrontendRestrictionContainer;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
 /***************************************************************
  *  Copyright notice
  *
- *  (c) 2015 Heiko Westermann <hwt3@gmx.de>
+ *  (c) 2015-2018 Heiko Westermann <hwt3@gmx.de>
  *  All rights reserved
  *
  *  This script is part of the TYPO3 project. The TYPO3 project is
@@ -77,60 +82,71 @@ class KeSearchHooks {
             // get the elements with frontend user group access restrictions
             // or time (start / stop) restrictions.
             // Copy those restrictions to the index.
+            $sysfolders = GeneralUtility::intExplode(',', $indexerConfig['storagepid'], true);
+            if ($sysfolders === '' || count($sysfolders) === 0) {
+                $sysfolders = null;
+            }
+
+
             $fields = '*';
             $table = 'tx_hwtaddress_domain_model_address';
-            $where = 'pid IN (' . $indexerConfig['sysfolder'] . ') AND hidden = 0 AND deleted = 0';
-            $groupBy = '';
-            $orderBy = '';
-            $limit = '';
-            $res = $GLOBALS['TYPO3_DB']->exec_SELECTquery($fields,$table,$where,$groupBy,$orderBy,$limit);
 
-            $resCount = $GLOBALS['TYPO3_DB']->sql_num_rows($res);
+            $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)
+                ->getQueryBuilderForTable($table);
+
+            //$queryBuilder->setRestrictions(GeneralUtility::makeInstance(FrontendRestrictionContainer::class));
+
+            $statement = $queryBuilder->select($fields)->from($table);
+            if ( $sysfolders ) {
+                $statement->where(
+                    $queryBuilder->expr()->in('pid', $sysfolders)
+                );
+            }
+            $statement = $statement->execute();
+
 
             // Loop through the records and write them to the index.
-            if($resCount) {
-                while ( ($record = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res)) ) {
+            while ( ($record = $statement->fetch()) ) {
+                // compile the information which should go into the index
+                // the field names depend on the table you want to index!
+                $title = strip_tags($record['firstname'] . "\n" . $record['lastname'] . "\n" . $record['company_title']);
+                $abstract = strip_tags($record['position'] . "\n" . $record['department'] . "\n" . $record['company_subtitle'] . "\n" . $record['company_short']);
+                $content = strip_tags($record['info'] . "\n" . $record['company_bodytext']);
+                $fullContent = $title . "\n" . $abstract . "\n" . $content;
+                $params = '&tx_hwtaddress_address[address]=' . $record['uid'];
+                //$tags = '#example_tag_1#,#example_tag_2#';
+                $additionalFields = array(
+                    'sortdate' => $record['crdate'],
+                    'orig_uid' => $record['uid'],
+                    'orig_pid' => $record['pid'],
+                    'sortdate' => $record['datetime'],
+                );
 
-                    // compile the information which should go into the index
-                    // the field names depend on the table you want to index!
-                    $title = strip_tags($record['firstname'] . "\n" . $record['lastname'] . "\n" . $record['company_title']);
-                    $abstract = strip_tags($record['position'] . "\n" . $record['department'] . "\n" . $record['company_subtitle'] . "\n" . $record['company_short']);
-                    $content = strip_tags($record['info'] . "\n" . $record['company_bodytext']);
-                    $fullContent = $title . "\n" . $abstract . "\n" . $content;
-                    $params = '&tx_hwtaddress_address[address]=' . $record['uid'];
-                    //$tags = '#example_tag_1#,#example_tag_2#';
-                    $additionalFields = array(
-                        'sortdate' => $record['crdate'],
-                        'orig_uid' => $record['uid'],
-                        'orig_pid' => $record['pid'],
-                        'sortdate' => $record['datetime'],
-                    );
+                // add something to the title, just to identify the entries
+                // in the frontend
+                $title = $title;
 
-                    // add something to the title, just to identify the entries
-                    // in the frontend
-                    $title = $title;
-
-                    // ... and store the information in the index
-                    $indexerObject->storeInIndex(
-                        $indexerConfig['storagepid'], // storage PID
-                        $title, // record title
-                        'hwtaddressindexer', // content type
-                        $indexerConfig['targetpid'], // target PID: where is the single view?
-                        $fullContent, // indexed content, includes the title (linebreak after title)
-                        $tags, // tags for faceted search
-                        $params, // typolink params for singleview
-                        $abstract, // abstract; shown in result list if not empty
-                        $record['sys_language_uid'], // language uid
-                        $record['starttime'], // starttime
-                        $record['endtime'], // endtime
-                        $record['fe_group'], // fe_group
-                        false, // debug only?
-                        $additionalFields // additionalFields
-                    );
-                }
-                $content = '<p><b>HWT Address Indexer "' . $indexerConfig['title'] . '": ' . $resCount . 'Elements have been indexed.</b></p>';
+                // ... and store the information in the index
+                $indexerObject->storeInIndex(
+                    $indexerConfig['storagepid'], // storage PID
+                    $title, // record title
+                    'hwtaddressindexer', // content type
+                    $indexerConfig['targetpid'], // target PID: where is the single view?
+                    $fullContent, // indexed content, includes the title (linebreak after title)
+                    $tags, // tags for faceted search
+                    $params, // typolink params for singleview
+                    $abstract, // abstract; shown in result list if not empty
+                    $record['sys_language_uid'], // language uid
+                    $record['starttime'], // starttime
+                    $record['endtime'], // endtime
+                    $record['fe_group'], // fe_group
+                    false, // debug only?
+                    $additionalFields // additionalFields
+                );
             }
-            return $content;
+
+            $content = '<p><b>HWT Address Indexer "' . $indexerConfig['title'] . '": ' . $resCount . 'Elements have been indexed.</b></p>';
         }
+        return $content;
     }
 }

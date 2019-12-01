56befc6b6d908e186abce32991a523ea644313e7
Heiko Westermann <westermann@tieup-media.de>
2019-12-01 14:57:31 +0100

    [UPDATE] Make extension available for TYPO3 10.x

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '0.2.2',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '9.0.0-9.5.99',
+            'typo3' => '9.0.0-10.2.99',
             'php' => '7.0.0-7.2.99',
         ),
         'conflicts' => array(

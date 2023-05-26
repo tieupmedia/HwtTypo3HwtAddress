fc6273764294369aac671ad576296cf29781cc9e
Philipp Kitzberger <coding@kitze.net>
2023-05-26 17:24:29 +0200

    Replace $_EXTKEY

--- ext_tables.php
+++ ext_tables.php
@@ -8,7 +8,7 @@ if (!defined('TYPO3_MODE')) {
 /*
  * CSH Files
  */
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_hwtaddress_domain_model_address', 'EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_csh_address.xlf');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_hwtaddress_domain_model_address', 'EXT:hwt_address/Resources/Private/Language/locallang_csh_address.xlf');
 
 /*
  * allow db record on standard pages

0c1848a6e38c8fe7836b2879c2cfe3d2f5561683
Heiko Westermann <westermann@tieup-media.de>
2024-01-26 16:00:42 +0100

    [BUGFIX] Replace $_EXTKEY in ext_tables.php for locallang path, broken in TYPO3 10.x

--- ext_tables.php
+++ ext_tables.php
@@ -4,11 +4,13 @@ if (!defined('TYPO3_MODE')) {
     die ('Access denied.');
 }
 
+$extensionKey = 'hwt_address';
+
 
 /*
  * CSH Files
  */
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_hwtaddress_domain_model_address', 'EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_csh_address.xlf');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_hwtaddress_domain_model_address', 'EXT:' . $extensionKey . '/Resources/Private/Language/locallang_csh_address.xlf');
 
 /*
  * allow db record on standard pages

80f240b1d5257683aaaeda0e476ecfeeb2c86a7a
Heiko Westermann <westermann@tieup-media.de>
2025-01-04 17:53:07 +0100

    [UPDATE] backend: Remove 'addLLrefForTCAdescr' calls for TYPO3 >= 13.0, breaking #100963, deprecation #97312

--- ext_tables.php
+++ ext_tables.php
@@ -8,11 +8,6 @@ $extensionKey = 'hwt_address';
 
 
 /*
- * CSH Files
- */
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_hwtaddress_domain_model_address', 'EXT:' . $extensionKey . '/Resources/Private/Language/locallang_csh_address.xlf');
-
-/*
  * allow db record on standard pages
  */
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_hwtaddress_domain_model_address');

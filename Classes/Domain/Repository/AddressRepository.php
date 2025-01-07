eb5f499f1dbedd712cdb6dba2bd179179327f4c7
Heiko Westermann <westermann@tieup-media.de>
2025-01-07 15:21:41 +0100

    [TASK] license: Update license notices and add license text

--- Classes/Domain/Repository/AddressRepository.php
+++ Classes/Domain/Repository/AddressRepository.php
@@ -2,32 +2,22 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the "hwt_address" Extension for TYPO3 CMS.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
 namespace Hwt\HwtAddress\Domain\Repository;
 
 use TYPO3\CMS\Extbase\Persistence\QueryInterface;
-
-/***************************************************************
- *  Copyright notice
- *
- *  (c) 2014-2019 Heiko Westermann <hwt3@gmx.de>
- *  All rights reserved
- *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
 
 /**
  * Address repository with all the callable functionality
